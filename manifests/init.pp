862a2bd088cb37bf66ad9160395e48c6dec6f0f9
Federico Voges <federico@puppetlabs.com>
2014-11-27 12:16:13 +0000

    Added user and group parameters to allow use with Puppet Enterprise (which uses pe-puppet instead of puppet) Also changed default config file to use $settings::confdir so it automatically uses the right path with Puppet open source and PE

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,13 +37,15 @@
 class logstash_reporter (
   $logstash_host  = '127.0.0.1',
   $logstash_port  = 5999,
-  $config_file  = '/etc/puppet/logstash.yaml',
+  $owner          = 'puppet',
+  $group          = 'puppet',
+  $config_file    = "${settings::confdir}/logstash.yaml",
 ){
 
   file { $config_file:
     ensure  => file,
-    owner   => 'puppet',
-    group   => 'puppet',
+    owner   => $owner,
+    group   => $group,
     mode    => '0444',
     content => template('logstash_reporter/logstash.yaml.erb'),
   }
